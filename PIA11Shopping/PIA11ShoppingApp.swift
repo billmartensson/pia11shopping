b7df46e239cac317641fd26cfba0319cb6265842
Bill Mårtensson <bill@magictechnology.se>
2022-10-24 09:08:51 +0200

    Initial Commit

---
+++ PIA11Shopping/PIA11ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIA11ShoppingApp.swift
+//  PIA11Shopping
+//
+//  Created by Bill Martensson on 2022-10-24.
+//
+
+import SwiftUI
+
+@main
+struct PIA11ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
